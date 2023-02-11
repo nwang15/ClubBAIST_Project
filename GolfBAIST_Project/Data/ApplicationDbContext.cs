1d0bc8896fa62931419ddacab67c1a2e15bbc668
nwang15 <nawang1019@gmail.com>
2023-02-11 13:28:31 -0700

    initial

---
+++ GolfBAIST_Project/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace GolfBAIST_Project.Data
+{   
+    public class ApplicationUser:IdentityUser
+    {
+        [StringLength(250)]
+        public string FirstName { get; set; }
+        [StringLength(250)]
+        public string LastName { get; set; }
+            
+    }
+    public class ApplicationDbContext : IdentityDbContext <ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+          : base(options)
+        {
+        }
+
+    }
+}
