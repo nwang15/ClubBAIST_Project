83903377322e7a37eccbbf383c09da5c3860339b
Anna Wang <35229351+nwang15@users.noreply.github.com>
2023-02-15 15:13:01 -0700

    page updated

--- GolfBAIST_Project/Models/Domain/MemberApplication.cs
+++ GolfBAIST_Project/Models/Domain/MemberApplication.cs
@@ -14,6 +14,20 @@ namespace GolfBAIST_Project.Models.Domain
         public string MembershipType { get; set; }
         public string MemberFirstName { get; set; }
         public string MemberLastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+        public string PostalCode { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public DateTime DOB { get; set; }
+        public string Occupation { get; set; }
+        public string CompanyName { get; set; }
+        public string CompanyAddress { get; set; }
+        public string CompanyCity { get; set; }
+        public string CompanyProvince { get; set; }
+        public string CompanyPostalCode { get; set; }
+        public string CompanyPhoneNumber { get; set; }
         public string MemberSignature { get; set; }
         public DateTime MemberSignDate { get; set; }
         public string FirstShareholderFullName { get; set; }
--- GolfBAIST_Project/Pages/ManageApplication/SubmitApplication.cshtml.cs
+++ GolfBAIST_Project/Pages/ManageApplication/SubmitApplication.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GolfBAIST_Project.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,6 +10,9 @@ namespace GolfBAIST_Project.Pages.ManageApplication
 {
     public class SubmitApplicationModel : PageModel
     {
+        [BindProperty]
+        public AddApplication AddApplicationRequest { get; set; }
+
         public void OnGet()
         {
         }
