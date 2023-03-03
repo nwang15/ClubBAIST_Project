7307bacd17f6b08929db0c227feef8ddfaec5635
nwang15 <nawang1019@gmail.com>
2023-03-02 22:16:33 -0700

    time picker updated

---
+++ GolfBAIST_Project/Pages/TimeTesting.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GolfBAIST_Project.Pages
+{
+    public class TimeTestingModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
